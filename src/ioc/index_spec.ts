fefb74177e3e209589f3559eb327ffbf64d39f04
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-09-27 14:30:10 +0200

    verify if ioc container can be mocked

--- src/ioc/index_spec.ts
+++ src/ioc/index_spec.ts
@@ -1,23 +1,43 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import * as path from 'path';
+import { join } from 'path';
+import { mock, SinonMock } from 'sinon';
 import { providers } from './ioc.providers';
 import { AngularCli } from '../lib/angular-cli.service';
 
-
-const collectionPath = path.join(__dirname, '../collection.json');
-
-
 describe('ioc', () => {
-  it('should add my sample file', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('ioc', {}, Tree.empty());
+  let collectionPath: string;
+  let runner: SchematicTestRunner;
+  let testProviders: SinonMock;
 
+  beforeAll(() => (collectionPath = join(__dirname, '../collection.json')));
+
+  beforeEach(() => {
+    runner = new SchematicTestRunner('schematics', collectionPath);
+  });
+
+  it('should add the file provided', () => {
+    const tree = runner.runSchematic('ioc', {}, Tree.empty());
     expect(tree.files).toContain('/src/app/flights-app.ts');
   });
 
+  it('should add another file provided by a mocked service', () => {
+    testProviders = mock(providers);
+    
+    testProviders.expects('get').withArgs(AngularCli).returns({
+      apps: [{ name: 'books-app', root: 'projects/books' }]
+    })
+    
+    const tree = runner.runSchematic('ioc', {}, Tree.empty());
+    expect(tree.files).toContain('/projects/books/books-app.ts');
+  });
+
+  afterEach(() => testProviders && testProviders.verify())
+});
+
+describe('injection-js', () => {
   it('should use injection-js', () => {
     const cli = providers.get(AngularCli);
     expect(cli).toBeDefined();
-  })
+  });
 });
